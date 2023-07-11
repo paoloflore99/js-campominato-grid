fbe041c70c336d9c2b6f057e6f60ae7f24f28138
paoloflore99 <paoloflore937@gmail.com>
2023-07-12 01:40:30 +0200

    non sono riuscito a capire i problemi, aprirotiket

--- myscript.js
+++ myscript.js
@@ -1,5 +1,3 @@
-"use strict"
-
 /*
 L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
@@ -25,11 +23,94 @@ const gridContainerQui = document.querySelector("#gridContainer");
 
 //vado a mettere il click sul btnjs
 btnJsQui.addEventListener("click", function () {
-    //console.log("ciao"); // funziona, viene visualizzato "ciao" sulla console dentro il click
+//console.log("ciao"); // funziona, viene visualizzato "ciao" sulla console dentro il click
 
-    const sqaureCounts = parseInt(document.querySelector("[name='sqaureCounts']").value);
-    console.log(sqaureCounts); //adesso  funziona, verrà visualizzato il valore del campo di input con l'attributo name="sqaureCounts" 100 81 49
+//bada bene ce lo sqaureCounts con s finale
+const sqaureCounts = parseInt(document.querySelector("[name='sqaureCounts']").value);
+console.log(sqaureCounts); //adesso  funziona, verrà visualizzato il valore del campo di input con l'attributo name="sqaureCounts" 100 81 49
 
+//torno fa sotto per creare una costanta
+const gridlist = creatoGriglia(sqaureCounts)  //RISOLVERE  problema qusto e un number ma deve essere un elemento html
+
+console.log(gridlist)
 //so quanti elementi vanno stampati
 //generazione griglia
+
+
+
+
+
+
+
+
+
+/**
+* questo mi serve per generare un quadrato
+* @param {string} sqaureCount
+* @returns {HTMLDivElement}
+*/
+function creatoQuadrato(sqaureCount) {
+    const square = document.createElement("div");
+
+    square.classList.add("grigliaquadrata");//errore qua per via delli spazi nelle parentesi
+    //square.classList.add("griglia quadrata");
+
+    //con confondere questo sqaureCount con quello con la s finale
+    //mi serve per mettere il contenuto internamente nel quadrato
+    square.textContent = sqaureCount
+
+    //questo e un autput
+    return square;
+
+}
+
+const quadrato = creatoQuadrato(sqaureCounts);
+gridContainerQui.appendChild(quadrato);
 });
+
+
+
+
+
+
+
+//sotto mi serve per creare gli elemeti html  , e poi li salvo in una variabbile per quando si fa click
+
+/**cosi facendo o convertito numeroQuadrati any a number
+* @param {number} numeroQuadrati  //qui i qudrati che vanno nella griglia attenzione al passagio
+*/
+function creatoGriglia(numeroQuadrati) {//adesso e un number grazie a riga sopra
+
+const grid = []
+
+//ciclo for
+for (let i = 0; i < numeroQuadrati; i++) {  //il validatore mi da errore sulla < che non deve avere spaziature        //numeroQuadrati non ha problemi
+
+
+    //perche non e una stringa ?
+    //salvo in una variabbile output della funzione creatoQuadrato
+    const nuovoQuadrato = creatoQuadrato("sqr" + i, numeroQuadrati )                               //a pensarci infatti lerrore e la ("qua" + i)                                    //problema
+
+    //cosi facendo salvo il quadrato
+    grid.push(nuovoQuadrato)                                                                                                                                        //trovato il problema e QUI
+
+
+}
+
+/*  non va bene mi stampa nel html solo  un  numero selezionato non tutti , quindi 100 mi stampo solo un numero 100 non 100 numeri 100
+for (let i = 0; i < sqaureCounts; i++) {
+    const quadrato = creatoQuadrato(i + 1);
+    gridContainerQui.appendChild(quadrato);
+}
+*/
+    //outup
+    return grid;   //any  
+
+
+}
+
+
+
+
+//turri i grid conterrano gli arrei
+//riga 33 e 92 problemi 
