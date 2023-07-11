917596ed39e78476f99cff0c749c513b4bcea1ba
paoloflore99 <paoloflore937@gmail.com>
2023-07-11 18:47:14 +0200

    vedo gli appunti prima

---
+++ myscript.js
@@ -0,0 +1,11 @@
+"use strict"
+
+/*
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+//prima di tutto per creare tutti i quadrati come faccio  eeeee
+
